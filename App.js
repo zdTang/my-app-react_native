32c6e215bc390794145cf924a9dea77c2af3f226
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2022-12-18 09:48:05 -0500

    component-view

--- App.js
+++ App.js
@@ -2,14 +2,21 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello world</Text>
+    <View style={[styles.container, {
+      // Try setting `flexDirection` to `"row"`.
+      flexDirection: "column"
+    }]}>
+      <View style={{ flex: 1, backgroundColor: "red" }} />
+      <View style={{ flex: 2, backgroundColor: "darkorange" }} />
+      <View style={{ flex: 3, backgroundColor: "green" }} />
     </View>
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-
+    flex: 1,
+    padding: 20,
   },
 });
