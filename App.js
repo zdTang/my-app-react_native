bb3323e5e8a531df1cec61e4a39407fb3de19404
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2022-12-18 10:47:15 -0500

    Others - Dimensions

--- App.js
+++ App.js
@@ -1,68 +1,45 @@
+import React, { useState, useEffect } from "react";
+import { View, StyleSheet, Text, Dimensions } from "react-native";
 
-//https://stackoverflow.com/questions/61014661/animated-usenativedriver-was-not-specified-issue-of-reactnativebase-input
-import React, { useRef } from "react";
-import { Animated, Text, View, StyleSheet, Button, SafeAreaView } from "react-native";
+const window = Dimensions.get("window");
+const screen = Dimensions.get("screen");
 
 const App = () => {
-  // fadeAnim will be used as the value for opacity. Initial Value: 0
-  const fadeAnim = useRef(new Animated.Value(0)).current;
+  const [dimensions, setDimensions] = useState({ window, screen });
 
-  const fadeIn = () => {
-    // Will change fadeAnim value to 1 in 5 seconds
-    Animated.timing(fadeAnim, {
-      toValue: 1,
-      duration: 5000,
-      useNativeDriver: true // Add This line
-    }).start();
-  };
-
-  const fadeOut = () => {
-    // Will change fadeAnim value to 0 in 3 seconds
-    Animated.timing(fadeAnim, {
-      toValue: 0,
-      duration: 3000,
-      useNativeDriver: true // Add This line
-    }).start();
-  };
+  useEffect(() => {
+    const subscription = Dimensions.addEventListener(
+      "change",
+      ({ window, screen }) => {
+        setDimensions({ window, screen });
+      }
+    );
+    return () => subscription?.remove();
+  });
 
   return (
-    <SafeAreaView style={styles.container}>
-      <Animated.View
-        style={[
-          styles.fadingContainer,
-          {
-            // Bind opacity to animated value
-            opacity: fadeAnim
-          }
-        ]}
-      >
-        <Text style={styles.fadingText}>Fading View!</Text>
-      </Animated.View>
-      <View style={styles.buttonRow}>
-        <Button title="Fade In View" onPress={fadeIn} />
-        <Button title="Fade Out View" onPress={fadeOut} />
-      </View>
-    </SafeAreaView>
+    <View style={styles.container}>
+      <Text style={styles.header}>Window Dimensions</Text>
+      {Object.entries(dimensions.window).map(([key, value]) => (
+        <Text>{key} - {value}</Text>
+      ))}
+      <Text style={styles.header}>Screen Dimensions</Text>
+      {Object.entries(dimensions.screen).map(([key, value]) => (
+        <Text>{key} - {value}</Text>
+      ))}
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center"
+    justifyContent: "center",
+    alignItems: "center"
   },
-  fadingContainer: {
-    padding: 20,
-    backgroundColor: "powderblue"
-  },
-  fadingText: {
-    fontSize: 28
-  },
-  buttonRow: {
-    flexBasis: 100,
-    justifyContent: "space-evenly",
-    marginVertical: 16
+  header: {
+    fontSize: 16,
+    marginVertical: 10
   }
 });
 
