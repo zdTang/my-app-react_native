469b807be51dd460a905111afd80e80d738fccec
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2022-12-18 10:55:47 -0500

    Others - PixelRatio

--- App.js
+++ App.js
@@ -1,82 +1,56 @@
-import React, { useState } from "react";
-import { Alert, Modal, StyleSheet, Text, Pressable, View } from "react-native";
+import React from "react";
+import { Image, PixelRatio, ScrollView, StyleSheet, Text, View } from "react-native";
 
-const App = () => {
-  const [modalVisible, setModalVisible] = useState(false);
-  return (
-    <View style={styles.centeredView}>
-      <Modal
-        animationType="slide"
-        transparent={true}
-        visible={modalVisible}
-        onRequestClose={() => {
-          Alert.alert("Modal has been closed.");
-          setModalVisible(!modalVisible);
-        }}
-      >
-        <View style={styles.centeredView}>
-          <View style={styles.modalView}>
-            <Text style={styles.modalText}>Hello World!</Text>
-            <Pressable
-              style={[styles.button, styles.buttonClose]}
-              onPress={() => setModalVisible(!modalVisible)}
-            >
-              <Text style={styles.textStyle}>Hide Modal</Text>
-            </Pressable>
-          </View>
-        </View>
-      </Modal>
-      <Pressable
-        style={[styles.button, styles.buttonOpen]}
-        onPress={() => setModalVisible(true)}
-      >
-        <Text style={styles.textStyle}>Show Modal</Text>
-      </Pressable>
-    </View>
-  );
+const size = 50;
+const cat = {
+  uri: "https://reactnative.dev/docs/assets/p_cat1.png",
+  width: size,
+  height: size
 };
 
+const App = () => (
+  <ScrollView style={styles.scrollContainer}>
+    <View style={styles.container}>
+      <Text>Current Pixel Ratio is:</Text>
+      <Text style={styles.value}>{PixelRatio.get()}</Text>
+    </View>
+    <View style={styles.container}>
+      <Text>Current Font Scale is:</Text>
+      <Text style={styles.value}>{PixelRatio.getFontScale()}</Text>
+    </View>
+    <View style={styles.container}>
+      <Text>On this device images with a layout width of</Text>
+      <Text style={styles.value}>{size} px</Text>
+      <Image source={cat} />
+    </View>
+    <View style={styles.container}>
+      <Text>require images with a pixel width of</Text>
+      <Text style={styles.value}>
+        {PixelRatio.getPixelSizeForLayoutSize(size)} px
+      </Text>
+      <Image
+        source={cat}
+        style={{
+          width: PixelRatio.getPixelSizeForLayoutSize(size),
+          height: PixelRatio.getPixelSizeForLayoutSize(size)
+        }}
+      />
+    </View>
+  </ScrollView>
+);
+
 const styles = StyleSheet.create({
-  centeredView: {
+  scrollContainer: {
     flex: 1,
+  },
+  container: {
     justifyContent: "center",
-    alignItems: "center",
-    marginTop: 22
+    alignItems: "center"
   },
-  modalView: {
-    margin: 20,
-    backgroundColor: "white",
-    borderRadius: 20,
-    padding: 35,
-    alignItems: "center",
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 4,
-    elevation: 5
-  },
-  button: {
-    borderRadius: 20,
-    padding: 10,
-    elevation: 2
-  },
-  buttonOpen: {
-    backgroundColor: "#F194FF",
-  },
-  buttonClose: {
-    backgroundColor: "#2196F3",
-  },
-  textStyle: {
-    color: "white",
-    fontWeight: "bold",
-    textAlign: "center"
-  },
-  modalText: {
-    marginBottom: 15,
-    textAlign: "center"
+  value: {
+    fontSize: 24,
+    marginBottom: 12,
+    marginTop: 4
   }
 });
 
