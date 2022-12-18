68f4bb2691a6dda304b8ebb25bd7398c38efa11f
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2022-12-18 11:53:47 -0500

    Props - Shadow Props

--- App.js
+++ App.js
@@ -1,153 +1,93 @@
-import React, { useState } from 'react';
-import { Button, ScrollView, StatusBar, StyleSheet, Text, View } from 'react-native';
 
-const App = () => {
-  const flexDirections = ['row', 'row-reverse', 'column', 'column-reverse'];
-  const justifyContents = [
-    'flex-start',
-    'flex-end',
-    'center',
-    'space-between',
-    'space-around',
-    'space-evenly',
-  ];
-  const alignItemsArr = [
-    'flex-start',
-    'flex-end',
-    'center',
-    'stretch',
-    'baseline',
-  ];
-  const wraps = ['nowrap', 'wrap', 'wrap-reverse'];
-  const directions = ['inherit', 'ltr', 'rtl'];
-  const [flexDirection, setFlexDirection] = useState(0);
-  const [justifyContent, setJustifyContent] = useState(0);
-  const [alignItems, setAlignItems] = useState(0);
-  const [direction, setDirection] = useState(0);
-  const [wrap, setWrap] = useState(0);
+// this props not works on my Andriod phone
+import React, { useState } from "react";
+import { Text, View, StyleSheet, Slider } from "react-native";
 
-  const hookedStyles = {
-    flexDirection: flexDirections[flexDirection],
-    justifyContent: justifyContents[justifyContent],
-    alignItems: alignItemsArr[alignItems],
-    direction: directions[direction],
-    flexWrap: wraps[wrap],
-  };
-
-  const changeSetting = (value, options, setterFunction) => {
-    if (value == options.length - 1) {
-      setterFunction(0);
-      return;
-    }
-    setterFunction(value + 1);
-  };
-  const [squares, setSquares] = useState([<Square />, <Square />, <Square />]);
+const ShadowPropSlider = ({ label, value, ...props }) => {
   return (
     <>
-      <View style={{ paddingTop: StatusBar.currentHeight }} />
-      <View style={[styles.container, styles.playingSpace, hookedStyles]}>
-        {squares.map(elem => elem)}
-      </View>
-      <ScrollView style={[styles.container]}>
-        <View style={[styles.controlSpace]}>
-          <View style={styles.buttonView}>
-            <Button
-              title="Change Flex Direction"
-              onPress={() =>
-                changeSetting(flexDirection, flexDirections, setFlexDirection)
-              }
-            />
-            <Text style={styles.text}>{flexDirections[flexDirection]}</Text>
-          </View>
-          <View style={styles.buttonView}>
-            <Button
-              title="Change Justify Content"
-              onPress={() =>
-                changeSetting(
-                  justifyContent,
-                  justifyContents,
-                  setJustifyContent
-                )
-              }
-            />
-            <Text style={styles.text}>{justifyContents[justifyContent]}</Text>
-          </View>
-          <View style={styles.buttonView}>
-            <Button
-              title="Change Align Items"
-              onPress={() =>
-                changeSetting(alignItems, alignItemsArr, setAlignItems)
-              }
-            />
-            <Text style={styles.text}>{alignItemsArr[alignItems]}</Text>
-          </View>
-          <View style={styles.buttonView}>
-            <Button
-              title="Change Direction"
-              onPress={() => changeSetting(direction, directions, setDirection)}
-            />
-            <Text style={styles.text}>{directions[direction]}</Text>
-          </View>
-          <View style={styles.buttonView}>
-            <Button
-              title="Change Flex Wrap"
-              onPress={() => changeSetting(wrap, wraps, setWrap)}
-            />
-            <Text style={styles.text}>{wraps[wrap]}</Text>
-          </View>
-          <View style={styles.buttonView}>
-            <Button
-              title="Add Square"
-              onPress={() => setSquares([...squares, <Square/>])}
-            />
-          </View>
-          <View style={styles.buttonView}>
-            <Button
-              title="Delete Square"
-              onPress={() =>
-                setSquares(squares.filter((v, i) => i != squares.length - 1))
-              }
-            />
-          </View>
-        </View>
-      </ScrollView>
+      <Text>
+        {label} ({value.toFixed(2)})
+      </Text>
+      <Slider step={1} value={value} {...props} />
     </>
   );
-};
+}
+
+const App = () => {
+  const [shadowOffsetWidth, setShadowOffsetWidth] = useState(0);
+  const [shadowOffsetHeight, setShadowOffsetHeight] = useState(0);
+  const [shadowRadius, setShadowRadius] = useState(0);
+  const [shadowOpacity, setShadowOpacity] = useState(0.1);
+
+  return (
+    <View style={styles.container}>
+      <View
+        style={[
+          styles.square,
+          {
+            shadowOffset: {
+              width: shadowOffsetWidth,
+              height: -shadowOffsetHeight
+            },
+            shadowOpacity,
+            shadowRadius
+          }
+        ]}
+      />
+      <View style={styles.controls}>
+        <ShadowPropSlider
+          label="shadowOffset - X"
+          minimumValue={-50}
+          maximumValue={50}
+          value={shadowOffsetWidth}
+          onValueChange={val => setShadowOffsetWidth(val)}
+        />
+        <ShadowPropSlider
+          label="shadowOffset - Y"
+          minimumValue={-50}
+          maximumValue={50}
+          value={shadowOffsetHeight}
+          onValueChange={val => setShadowOffsetHeight(val)}
+        />
+        <ShadowPropSlider
+          label="shadowRadius"
+          minimumValue={0}
+          maximumValue={100}
+          value={shadowRadius}
+          onValueChange={val => setShadowRadius(val)}
+        />
+        <ShadowPropSlider
+          label="shadowOpacity"
+          minimumValue={0}
+          maximumValue={1}
+          step={0.05}
+          value={shadowOpacity}
+          onValueChange={val => setShadowOpacity(val)}
+        />
+      </View>
+    </View>
+  );
+}
 
 const styles = StyleSheet.create({
   container: {
-    height: '50%',
+    flex: 1,
+    justifyContent: "space-around",
+    backgroundColor: "#ecf0f1",
+    padding: 8
   },
-  playingSpace: {
-    backgroundColor: 'white',
-    borderColor: 'blue',
-    borderWidth: 3,
+  square: {
+    alignSelf: "center",
+    backgroundColor: "white",
+    borderRadius: 4,
+    height: 150,
+    shadowColor: "black",
+    width: 150
   },
-  controlSpace: {
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    backgroundColor: '#F5F5F5',
-  },
-  buttonView: {
-    width: '50%',
-    padding: 10,
-  },
-  text: { textAlign: 'center' },
+  controls: {
+    paddingHorizontal: 12
+  }
 });
-
-const Square = () => (
-  <View style={{
-    width: 50,
-    height: 50,
-    backgroundColor: randomHexColor(),
-  }} />
-);
-
-const randomHexColor = () => {
-  return '#000000'.replace(/0/g, () => {
-    return (~~(Math.random() * 16)).toString(16);
-  });
-};
 
 export default App;
