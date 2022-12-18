32db150fb2dca1e79bd00ea7a49e41e444e1bc1b
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2022-12-18 11:48:20 -0500

    props - Image Style Props

--- App.js
+++ App.js
@@ -1,86 +1,77 @@
-import React, { useState } from 'react';
-import { Button, Platform, SafeAreaView, StatusBar, StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { View, Image, Text, StyleSheet } from "react-native";
 
-const STYLES = ['default', 'dark-content', 'light-content'];
-const TRANSITIONS = ['fade', 'slide', 'none'];
-
-const App = () => {
-  const [hidden, setHidden] = useState(false);
-  const [statusBarStyle, setStatusBarStyle] = useState(STYLES[0]);
-  const [statusBarTransition, setStatusBarTransition] = useState(TRANSITIONS[0]);
-
-  const changeStatusBarVisibility = () => setHidden(!hidden);
-
-  const changeStatusBarStyle = () => {
-    const styleId = STYLES.indexOf(statusBarStyle) + 1;
-    if (styleId === STYLES.length) {
-      setStatusBarStyle(STYLES[0]);
-    } else {
-      setStatusBarStyle(STYLES[styleId]);
-    }
-  };
-
-  const changeStatusBarTransition = () => {
-    const transition = TRANSITIONS.indexOf(statusBarTransition) + 1;
-    if (transition === TRANSITIONS.length) {
-      setStatusBarTransition(TRANSITIONS[0]);
-    } else {
-      setStatusBarTransition(TRANSITIONS[transition]);
-    }
-  };
-
+const DisplayAnImageWithStyle = () => {
   return (
-    <SafeAreaView style={styles.container}>
-      <StatusBar
-        animated={true}
-        backgroundColor="#61dafb"
-        barStyle={statusBarStyle}
-        showHideTransition={statusBarTransition}
-        hidden={hidden} />
-      <Text style={styles.textStyle}>
-        StatusBar Visibility:{'\n'}
-        {hidden ? 'Hidden' : 'Visible'}
-      </Text>
-      <Text style={styles.textStyle}>
-        StatusBar Style:{'\n'}
-        {statusBarStyle}
-      </Text>
-      {Platform.OS === 'ios' ? (
-        <Text style={styles.textStyle}>
-          StatusBar Transition:{'\n'}
-          {statusBarTransition}
-        </Text>
-      ) : null}
-      <View style={styles.buttonsContainer}>
-        <Button
-          title="Toggle StatusBar"
-          onPress={changeStatusBarVisibility} />
-        <Button
-          title="Change StatusBar Style"
-          onPress={changeStatusBarStyle} />
-        {Platform.OS === 'ios' ? (
-          <Button
-            title="Change StatusBar Transition"
-            onPress={changeStatusBarTransition} />
-        ) : null}
+    <View style={styles.container}>
+      <View>
+        <Image
+          style={{
+            resizeMode: "cover",
+            height: 100,
+            width: 200
+          }}
+          source={require("./assets/icon.png")}
+        />
+        <Text>resizeMode : cover</Text>
       </View>
-    </SafeAreaView>
+      <View>
+        <Image
+          style={{
+            resizeMode: "contain",
+            height: 100,
+            width: 200
+          }}
+          source={require("./assets/icon.png")}
+        />
+        <Text>resizeMode : contain</Text>
+      </View>
+      <View>
+        <Image
+          style={{
+            resizeMode: "stretch",
+            height: 100,
+            width: 200
+          }}
+          source={require("./assets/icon.png")}
+        />
+        <Text>resizeMode : stretch</Text>
+      </View>
+      <View>
+        <Image
+          style={{
+            resizeMode: "repeat",
+            height: 100,
+            width: 200
+          }}
+          source={require("./assets/icon.png")}
+        />
+        <Text>resizeMode : repeat</Text>
+      </View>
+      <View>
+        <Image
+          style={{
+            resizeMode: "center",
+            height: 100,
+            width: 200
+          }}
+          source={require("./assets/icon.png")}
+        />
+        <Text>resizeMode : center</Text>
+      </View>
+    </View>
   );
-};
+}
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    backgroundColor: '#ECF0F1'
-  },
-  buttonsContainer: {
-    padding: 10
-  },
-  textStyle: {
-    textAlign: 'center',
-    marginBottom: 8
+    display: "flex",
+    flexDirection: null,// here change to null from original "vertical"
+    justifyContent: "space-around",
+    alignItems: "center",
+    height: "100%",
+    textAlign: "center"
   }
 });
 
-export default App;
+export default DisplayAnImageWithStyle;
