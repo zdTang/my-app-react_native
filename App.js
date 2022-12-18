577c7ac5c4c938721d1e434a7b452144a90fbf1a
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2022-12-18 10:36:45 -0500

    others - ActivityIndicator

--- App.js
+++ App.js
@@ -1,51 +1,24 @@
 import React from "react";
-import { View, StyleSheet, ToastAndroid, Button, StatusBar } from "react-native";
+import { ActivityIndicator, StyleSheet, Text, View } from "react-native";
 
-const App = () => {
-  const showToast = () => {
-    ToastAndroid.show("A pikachu appeared nearby !", ToastAndroid.SHORT);
-  };
-
-  const showToastWithGravity = () => {
-    ToastAndroid.showWithGravity(
-      "All Your Base Are Belong To Us",
-      ToastAndroid.SHORT,
-      ToastAndroid.CENTER
-    );
-  };
-
-  const showToastWithGravityAndOffset = () => {
-    ToastAndroid.showWithGravityAndOffset(
-      "A wild toast appeared!",
-      ToastAndroid.LONG,
-      ToastAndroid.BOTTOM,
-      25,
-      50
-    );
-  };
-
-  return (
-    <View style={styles.container}>
-      <Button title="Toggle Toast" onPress={() => showToast()} />
-      <Button
-        title="Toggle Toast With Gravity"
-        onPress={() => showToastWithGravity()}
-      />
-      <Button
-        title="Toggle Toast With Gravity & Offset"
-        onPress={() => showToastWithGravityAndOffset()}
-      />
-    </View>
-  );
-};
+const App = () => (
+  <View style={[styles.container, styles.horizontal]}>
+    <ActivityIndicator />
+    <ActivityIndicator size="large" />
+    <ActivityIndicator size="small" color="#0000ff" />
+    <ActivityIndicator size="large" color="#00ff00" />
+  </View>
+);
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    paddingTop: StatusBar.currentHeight,
-    backgroundColor: "#888888",
-    padding: 8
+    justifyContent: "center"
+  },
+  horizontal: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    padding: 10
   }
 });
 
