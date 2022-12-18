332b68dd245a271987347d858953df059aad74f2
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2022-12-18 12:44:52 -0500

    setup folder hierarchy for a new project
    create a decent folder structure, and clear unnecessary code for preparing a brand-new project

--- App.js
+++ App.js
@@ -1,43 +1,19 @@
 import React from "react";
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet,Text } from "react-native";
 
 const ViewStyleProps = () => {
     return (
       <View style={styles.container}>
-        <View style={styles.top} />
-        <View style={styles.middle} />
-        <View style={styles.bottom} />
+        <Text> hello Mike</Text>
       </View>
     );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: "space-between",
-    backgroundColor: "#fff",
-    padding: 20,
-    margin: 10,
+    padding:50
   },
-  top: {
-    flex: 0.3,
-    backgroundColor: "grey",
-    borderWidth: 5,
-    borderTopLeftRadius: 20,
-    borderTopRightRadius: 20,
-  },
-  middle: {
-    flex: 0.3,
-    backgroundColor: "beige",
-    borderWidth: 5,
-  },
-  bottom: {
-    flex: 0.3,
-    backgroundColor: "pink",
-    borderWidth: 5,
-    borderBottomLeftRadius: 20,
-    borderBottomRightRadius: 20,
-  },
+  
 });
 
 export default ViewStyleProps;
